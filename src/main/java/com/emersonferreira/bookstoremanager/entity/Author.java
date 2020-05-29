dcaa59d651cf822eb99274498ff981ab4d1971a6
Emerson Ferreira da Silva <emersonfdasilva2@gmail.com>
2020-05-28 23:05:01 -0300

    Developed project entites

---
+++ src/main/java/com/emersonferreira/bookstoremanager/entity/Author.java
@@ -0,0 +1,24 @@
+package com.emersonferreira.bookstoremanager.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer age;
+
+}
